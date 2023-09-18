a8062e232962a3e1fc589110d750347b04110213
danielthetam <danieltzytzytzy@gmail.com>
2023-09-18 21:08:36 +0800

    update

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230918.125134';
+const cacheName = 'chirpy-20230918.174723';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
