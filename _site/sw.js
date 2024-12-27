2728d28b873c59ec9204f006727e991d39c14891
Daniel Tam <danieltzytzytzy@gmail.com>
2024-12-27 16:43:11 +0800

    Added documentation for GCxTrelloIntegration

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1735287861';
+const cacheName = 'chirpy-1735288956';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
