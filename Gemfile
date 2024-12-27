e80f39a24cc0d5976f0d531c55a6c397437cc80a
Daniel Tam <danieltzytzytzy@gmail.com>
2024-12-27 16:48:26 +0800

    Added documentation for GCxTrelloIntegration

--- Gemfile
+++ Gemfile
@@ -28,3 +28,7 @@ if RUBY_PLATFORM =~ /linux-musl/
 end
 
 gem "webrick", "~> 1.8"
+
+gem "csv", "~> 3.3"
+
+gem "logger", "~> 1.6"
--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1735288956';
+const cacheName = 'chirpy-1735289279';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
